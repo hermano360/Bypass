745a6838e3baee4a17a2bcf05c896c6aef8501b1
Roy Lee <royglee@gmail.com>
2016-08-13 19:24:31 -0700

    clean up metadata file and app.js

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 	res.status(200).render('esri_testing');
 });
 
-
-app.use(express.static(__dirname + '/public'));
-app.use('/static', express.static(__dirname + '/public'));
-
 app.listen(port);
 console.log("Listening to port", port);
-
-
